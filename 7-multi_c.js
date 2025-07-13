fd290e1a27ae8aeb5f87a47ac0644a1d575beb27
Bright <bright123cb@gmail.com>
2025-07-13 09:13:25 -0700

    Added script to print C is fun x times

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,11 +1,13 @@
 const args = process.argv.slice(2);
+const times = parseInt(args[0]);
 
-if (isNaN(args[0]) || args[0] <= 0) {
+if (isNaN(times)) {
   console.log("Missing number of occurrences");
 } else {
   let output = '';
-  for (let i = 0; i < parseInt(args[0]); i++) {
+  for (let i = 0; i < Math.max(times, 0); i++) {
     output += "C is fun\n";
   }
   console.log(output.trim());
 }
+
